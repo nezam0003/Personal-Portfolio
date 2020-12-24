ec890d46e3be3b9ffdfa3682abbc5063024d8fa4
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-24 14:08:20 +0600

    will start from here

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.menu');
+
+navToggle.addEventListener('click', () => {
+
+});
