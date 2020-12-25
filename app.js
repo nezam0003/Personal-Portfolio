9a57e320a819df79ea26520f8a09b621c09e80b2
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-25 11:25:04 +0600

    navbar toggle scroll events manipulated in app.js

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = document.querySelector('.links-container');
-const links = document.querySelector('.menu');
+const linksContainer = document.querySelector('.items-container');
+const links = document.querySelector('.nav-items');
 
+// Toggle events
 navToggle.addEventListener('click', () => {
+    const linksContainerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    if (linksContainerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+});
 
+// Target Navbar & Scroll-Link
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+// Fixed navbar
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+    if (scrollHeight > 400) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
 });
