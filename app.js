19bb450671dcc37ff93d250015ea873bbc53f6a9
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-29 11:07:30 +0600

    Dynamic typeWriter using prototype method app.js

--- app.js
+++ app.js
@@ -62,3 +62,73 @@ scrollLinks.forEach((link) => {
         linksContainer.style.height = 0;
     });
 });
+
+// Typing Effect
+const TypeWriter = function(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+    // type method
+TypeWriter.prototype.type = function() {
+    // current index of word
+    const current = this.wordIndex % this.words.length;
+
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if Deleting
+    if (this.isDeleting) {
+        // Remove character
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+
+    } else {
+        // Add character
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type speed
+    let typeSpeed = 300;
+    if (this.isDeleting) {
+        typeSpeed /= 2;
+    }
+
+    // if Word is completed
+    if (!this.isDeleting && this.txt === fullTxt) {
+        // this will make pause at the End
+        typeSpeed = this.wait;
+        // set isDeleting to True
+        this.isDeleting = true;
+    }
+    // Switch to the next word after deleting one
+    else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        // Pause before start typing again
+        typeSpeed = 500;
+
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+}
+
+// init on DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// init App
+function init() {
+    const txtElement = document.querySelector('.text-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    // Initialize TypeWriter
+    new TypeWriter(txtElement, words, wait);
+
+}
